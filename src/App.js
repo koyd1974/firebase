09536f0b3f1a54f1124ac4a11445f414f70a5fac
koyd9174 <koyd9174@naver.com>
2022-09-30 21:09:54 +0900

    9/30 firebase

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { firestore } from './firebase';
+import { useEffect } from 'react';
 
 function App() {
+
+  useEffect(() => {
+    // bucket이라는 변수로 firestore의 collection인 bucket에 접근
+    const user = firestore.collection("user");
+
+    // collection의 document인 "bucket_item"을 가져온다.
+    user.doc("user").get().then((doc) => {
+
+      // document의 데이터를 가져옴
+      console.log(doc.data());
+      // document의 id를 가져옴
+      console.log(doc.id);
+    });
+  });
+
   return (
     <div className="App">
-tset1231231231223
+      firebase 확인해보기!
     </div>
   );
 }
